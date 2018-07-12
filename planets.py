1d0762b6ca508b7195ba17384dc8482bed123658
TorroesPrime <michael.cividanes2014@gmail.com>
2018-07-11 23:38:49 -0400

    Create planets.py

---
+++ planets.py
@@ -0,0 +1,46 @@
+from classes import planet
+population= 'Estimated 2 Billion'
+tithe = 'Exactus Non'
+compliance ='Undergoing integration into the Imperium vis the Achilus Crusade'
+demography ='''Hot/Temperate, abiding planetary climate is
+tropical with a low degree of precipitation and storm activity. The planet has
+two main continental landmasses. Each are dominated by arid plains and rain
+forest zones surrounding a chain of large mountain ranges. The equatorial
+regions are made up of bands of deserts. The planet also possesses relatively
+small polar oceans.'''
+gov_type='Indigenous Monarchy (reigning ruler is known as "The Caele")'
+governor='Not yet determined'
+adept='Initial/Low; the Ecclesiarchy and Imperial Guard have small stations set up on the planet’s main continent.'
+military='The Aurum tribes have a primitive force of infantry and cavalry (roughly two regiments of each), nominally commanded by the Caele.'
+trade='''Aurum is rich in promethium deposits and the rare decavane crystals (see below). In addition to its abundant natural resources, the planet is strategically well-suited to the needs of the Crusade and the Aurum tribes themselves are a fierce warrior culture that is nearly ideal for integration into the Imperial Guard.'''
+codex='''Aurum derives its name from the golden glow that suffuses its atmosphere. If it contained so mundane a resource as
+actual gold ore, it would be of interest only to reckless Rogue Traders willing to risk the untamed Jericho Reach to
+exploit it. However, Aurum is rich in a far rarer and more immediately useful resource than metal. The unique amber
+colour suffusing the sky and tinting the planet originates from its rare Decavane crystals. Decavane crystals can hold an
+exponentially larger charge than standard crystal batteries, making them highly valued for alternate weapon patterns
+and other military equipment.
+The relationship between the crystals and Aurum’s environment, if any, is unclear, but it is obvious the planet is
+exceptionally resource-rich and fertile. Nearly every form of flora prospers when introduced to the rich soil, and herd
+animals mature quickly and free of illness. Even the native population has grown strong and able on this unusually
+healthy planet, although the native predators are equally flourishing and the Aurans must strive constantly to perfect
+their martial skills to keep pace with them.) Some have theorised that the Decavane crystals emit a subtle, beneficial
+energy field, while others propose that they are the effect and not the cause of some undiscovered property of the planet.
+Until the natives allow the Adeptus Mechanicus to perform detailed research, scholars can only conjecture.
+Imperial Roots
+For their part, the Aurans attribute their planet’s benevolence to the “Soul of Aurum,” a male personification of the
+planet’s will. This Soul actually has striking similarities to the God-Emperor—from his creation of 20 sons and their
+armies to a betrayal by his most beloved child. Their religious parables likewise show roots in the Imperial Creed. The
+original date of human habitation can only be guessed at, but the low gothic dialect and various cultural artefacts clearly
+indicate an Imperial history.
+Culture
+Combat is an Auran way of life, encompassing all ages and genders. In the planet’s past, scattered tribes warred for
+dominance. The tribes have been united for several centuries, but the planet’s predators ensure that martial prowess
+remains a necessity. As a result, while the Aurans do farm the fertile land and raise herd animals, traditional hunts
+remains a staple of their culture. The society has not yet developed currency; they rely on a barter system.
+The traditional Auran greeting is to touch one’s weapon’s hilt to the other party’s shoulder. Non-lethal martial
+challenges are used to solve nearly any disagreement, from domestic quarrels to trade disputes. If a matter escalates to
+a serious grudge, the natives have a tradition called “splitting the rock of grievance.” 
+Aurum’s plains make wood an uncommon commodity, and it is unclear if metal ore even exists on the planet. The
+natives show a relatively advanced understanding of farming and medicine, but their implements tend to be made of
+rock and bone.'''
+planet01 = planet('Aurum',population,tithe,compliance,demography,gov_type,governor,adept,military,trade,codex)
