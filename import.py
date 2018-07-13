8d83f5c7123c1de236e21c7b49edbc53ee900d90
TorroesPrime <michael.cividanes2014@gmail.com>
2018-07-12 21:49:46 -0400

    Update import.py

--- import.py
+++ import.py
@@ -3,9 +3,11 @@ workbook = xlrd.open_workbook("library.xlsx")
 worksheet = workbook.sheet_by_name("names")
 HumanMaleNames = []
 print('the name at column 1, row 20 is: '+worksheet.cell(4,2).value)
-iteration = 1
-j = worksheet.cell(1,iteration).value
+row = 1
+j = worksheet.cell(row,1).value
 while j != xlrd.empty_cell.value:
     HumanMaleNames.append(j)
-    iteration += 1
-    print(str(iteration)+'name added to list')
+    row += 1
+    print(str(j)+' added to list')
+    print(str(row))
+
