677ee442011c1013e06942aea9747d27601a5fed
TorroesPrime <michael.cividanes2014@gmail.com>
2018-07-17 12:32:30 -0400

    Delete import2.py

--- import2.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from pandas import ExcelWriter
-from pandas import ExcelFile
-
-source = pd.read_excel('library.xlsx', sheetname='names')
-print("Column Headings:")
-print(source.columns)
