fe8e7baaece74cfe218ed3a8032e3608c634203e
TorroesPrime <michael.cividanes2014@gmail.com>
2018-09-27 01:00:49 -0400

    working on full restart of project including list reader program. Completed program to take text file and read it into a spreadsheet - MGC

--- ObjectsandClassesLibrary.py
+++ ObjectsandClassesLibrary.py
@@ -40,41 +40,41 @@ class character(object):
     """Character class.
 
     The basic character class. Each character has a string for their name (name), 10 basic characteristics
-    including Weapon Skill (Stat_WS), Ballistic skill (Stat_BS), Strength(Stat_Strength),
-    Toughness(Stat_Toughness), Agility(Stat_Agility),Intelligence(Stat_Int),
-    Perception(Stat_Perc), Fellowship(Stat_Fell), fatigue, and wounds. the 'Stats' are copied into the
-    Char_Stats dictionary to make it easier to run through all the stats when making a stat check or test."""
-    def __init__ (self, race, gender, Fname, Lname, Stat_WS, Stat_BS, Stat_Strength, Stat_Tough, Stat_Agility, Stat_Int, Stat_Perc, Stat_WP, Stat_Fell, wounds):
+    including Weapon Skill (stats_weapon_skill), Ballistic skill (stats_ballistic_skill), Strength(stat_strength),
+    Toughness(stat_toughness), Agility(stat_agility),Intelligence(stat_int),
+    Perception(stat_perc), Fellowship(stat_fellowship), fatigue, and wounds. the 'Stats' are copied into the
+    char_stats dictionary to make it easier to run through all the stats when making a stat check or test."""
+    def __init__ (self, race, gender, firstname, lastname, stats_weapon_skill, stats_ballistic_skill, stat_strength, stat_tough, stat_agility, stat_int, stat_perc, stat_will_power, stat_fellowship, wounds):
         self.race = race
-        self.raceBonus = 0
+        self.racebonus = 0
         self.gender = gender
-        self.Fname = Fname
-        self.Lname = Lname
-        self.FullName = Fname+" "+Lname
-        self.Char_Stats = {'Weapon Skill':0,'Ballistic Skill':0,'Strength':0,'Toughness':0,'Agility':0,'Intelligence':0,'Perception':0,'Will Power':0,'Fellowship':0,}
-        self.Stat_WS = Stat_WS
-        self.Stat_BS = Stat_BS
-        self.Stat_Strength = Stat_Strength
-        self.Stat_Tough = Stat_Tough
-        self.Stat_Agility = Stat_Agility
-        self.Stat_Int = Stat_Int
-        self.Stat_Perc = Stat_Perc
-        self.Stat_WP = Stat_WP
-        self.Stat_Fell = Stat_Fell
-        self.Char_Stats['Weapon Skill'] = int(Stat_WS)
-        self.Char_Stats['Ballistic Skill'] = int(self.Stat_BS)
-        self.Char_Stats['Strength'] = int(self.Stat_Strength)
-        self.Char_Stats['Toughness'] = int(self.Stat_Tough)
-        self.Char_Stats['Agility'] = int(self.Stat_Agility)
-        self.Char_Stats['Intelligence'] = int(self.Stat_Int)
-        self.Char_Stats['Perception'] = int(self.Stat_Perc)
-        self.Char_Stats['Will Power'] = int(self.Stat_WP)
-        self.Char_Stats['Fellowship'] = int(self.Stat_Fell)
+        self.firstname = firstname
+        self.lastname = lastname
+        self.Fullastname = firstname+" "+lastname
+        self.char_stats = {'Weapon Skill':0,'Ballistic Skill':0,'Strength':0,'Toughness':0,'Agility':0,'Intelligence':0,'Perception':0,'Will Power':0,'Fellowship':0,}
+        self.stats_weapon_skill = stats_weapon_skill
+        self.stats_ballistic_skill = stats_ballistic_skill
+        self.stat_strength = stat_strength
+        self.stat_tough = stat_tough
+        self.stat_agility = stat_agility
+        self.stat_int = stat_int
+        self.stat_perc = stat_perc
+        self.stat_will_power = stat_will_power
+        self.stat_fellowship = stat_fellowship
+        self.char_stats['Weapon Skill'] = int(stats_weapon_skill)
+        self.char_stats['Ballistic Skill'] = int(self.stats_ballistic_skill)
+        self.char_stats['Strength'] = int(self.stat_strength)
+        self.char_stats['Toughness'] = int(self.stat_tough)
+        self.char_stats['Agility'] = int(self.stat_agility)
+        self.char_stats['Intelligence'] = int(self.stat_int)
+        self.char_stats['Perception'] = int(self.stat_perc)
+        self.char_stats['Will Power'] = int(self.stat_will_power)
+        self.char_stats['Fellowship'] = int(self.stat_fellowship)
         self.fatigue = 0
         self.wounds = wounds
         
    # def StatBuilder(racebonus):
-   #     stat = int(random.randint(1,20+raceBonus)
+   #     stat = int(random.randint(1,20+racebonus)
    #     return stat
     @classmethod
     def AutoMaker(cls, type):
@@ -83,53 +83,53 @@ class character(object):
         Automatic constructor. It just automatically builds a character object when called."""
         if type == "Human":
             race = 'human'
-            raceBonus = 25
+            racebonus = 25
         else:
             race = 'human'
-            raceBonus = 20
-        #print(raceBonus)
+            racebonus = 20
+        #print(racebonus)
         #print(race)
         coinflip = random.randint(1,2)
         if coinflip == 1:
             gender = 'Male'
         else:
             gender = 'Female'
-        Stat_WS = int(random.randint(1,20)+raceBonus)
-        Stat_BS = int(random.randint(1,20)+raceBonus)
-        Stat_Strength = int(random.randint(1,20)+raceBonus)
-        Stat_Tough = int(random.randint(1,20)+raceBonus)
-        Stat_Agility = int(random.randint(1,20)+raceBonus)
-        Stat_Int = int(random.randint(1,20)+raceBonus)
-        Stat_Perc = int(random.randint(1,20)+raceBonus)
-        Stat_WP = int(random.randint(1,20)+raceBonus)
-        Stat_Fell = int(random.randint(1,20)+raceBonus)
+        stats_weapon_skill = int(random.randint(1,20)+racebonus)
+        stats_ballistic_skill = int(random.randint(1,20)+racebonus)
+        stat_strength = int(random.randint(1,20)+racebonus)
+        stat_tough = int(random.randint(1,20)+racebonus)
+        stat_agility = int(random.randint(1,20)+racebonus)
+        stat_int = int(random.randint(1,20)+racebonus)
+        stat_perc = int(random.randint(1,20)+racebonus)
+        stat_will_power = int(random.randint(1,20)+racebonus)
+        stat_fellowship = int(random.randint(1,20)+racebonus)
         wounds = random.randint(1,10)+18
         fatigue = 0
         if gender == 'Male':
-            Fname = random.choice(HumanMaleNames)
+            firstname = random.choice(HumanMaleNames)
         else:
-            Fname = random.choice(HumanFemaleNames)
-        Lname = random.choice(HumanLastNames)
+            firstname = random.choice(HumanFemaleNames)
+        lastname = random.choice(HumanLastNames)
 #returns a character Object with the values generated and selected.
-        return character(race, gender, Fname, Lname, Stat_WS, Stat_BS, Stat_Strength, Stat_Tough, Stat_Agility, Stat_Int, Stat_Perc, Stat_WP, Stat_Fell, wounds)
+        return character(race, gender, firstname, lastname, stats_weapon_skill, stats_ballistic_skill, stat_strength, stat_tough, stat_agility, stat_int, stat_perc, stat_will_power, stat_fellowship, wounds)
 
     def character_sheet(self):
         """Character sheet Generation function. Outputs a charactersheet with the characteristics."""
         print(divider)
-        print("Character Name: "+self.FullName+"    Character Race: "+self.race+"     Gender: "+self.gender)
+        print("Character Name: "+self.Fullastname+"    Character Race: "+self.race+"     Gender: "+self.gender)
         print("Character Wounds: "+str(self.wounds)+"         Character Fatigue: "+str(self.fatigue))
         print(divider)
         print(div_char)
-        print(" WS:{WS}  BS:{BS}    S:{S}    T:{T}   Ag:{Ag}   Int:{Int}  Per:{Per}   WP:{WP}   Fel:{Fel}      ".format(WS=self.Stat_WS, BS=self.Stat_BS, S=self.Stat_Strength , T=self.Stat_Tough , Ag=self.Stat_Agility, Int=self.Stat_Int,Per=self.Stat_Perc,WP=self.Stat_WP,Fel=self.Stat_Fell))
+        print(" WS:{WS}  BS:{BS}    S:{S}    T:{T}   Ag:{Ag}   Int:{Int}  Per:{Per}   WP:{WP}   Fel:{Fel}      ".format(WS=self.stats_weapon_skill, BS=self.stats_ballistic_skill, S=self.stat_strength , T=self.stat_tough , Ag=self.stat_agility, Int=self.stat_int,Per=self.stat_perc,WP=self.stat_will_power,Fel=self.stat_fellowship))
         print(divider)
         for skill in skills:
             print
         print(pageBreak)
 
-#        print(spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS)+spacer+str(self.Stat_WS))
+#        print(spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill)+spacer+str(self.stats_weapon_skill))
 class RogueTrader(character):
     race = 'human'
-    raceBonus = random.randint(20,28)
+    racebonus = random.randint(20,28)
     def character_sheet(self):
         print('yes')
         #pass
